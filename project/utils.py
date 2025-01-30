12737e361a1a604642b39b33c32dacd576ef2501
grash002 <grash002@yandex.ru>
2025-01-30 20:37:10 +0300

    add logging

--- project/utils.py
+++ project/utils.py
@@ -1,16 +1,17 @@
 import logging
-from project.models import Log  # Импортируем модель, которую создали ранее
+from django.db import models
 
+# Обработчик логов для записи в базу данных
 class DatabaseLogHandler(logging.Handler):
     def emit(self, record):
         try:
-            # Формируем сообщение лога
+            from project.models import Log  # Импортируем модель в момент использования
             log_message = self.format(record)
             level = record.levelname
             message = record.getMessage()
 
-            # Записываем лог в базу данных
+            # Записываем лог в БД
             Log.objects.create(level=level, message=message)
         except Exception as e:
-            # Если произошла ошибка при записи в БД, можно записать её в стандартный лог
+            # Если возникла ошибка при записи в БД, пишем в стандартный лог
             print(f"Error logging to database: {e}")
