4f6d0cffefa80ef4f87ba8d9fa3070e9a731c91f
grash002 <grash002@yandex.ru>
2025-01-30 20:49:05 +0300

    add logging

--- project/models.py
+++ project/models.py
@@ -14,7 +14,7 @@ class Log(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     request_path = models.CharField(max_length=255, null=True, blank=True)
     request_method = models.CharField(max_length=10, null=True, blank=True)
-    user = models.CharField(max_length=255, null=True, blank=True)
+    user_name = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return f'{self.level} - {self.timestamp} - {self.message}'
